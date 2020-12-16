d1e8dad29d668461442c48aa31b7bdf79cbd9d12
Harishkarthik <gunalan2@illinois.edu>
2020-12-16 11:06:12 -0500

    Add files via upload

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,23 @@
+const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+const FlightSuretyData = artifacts.require("FlightSuretyData");
+const fs = require('fs');
+
+module.exports = function(deployer) {
+
+    let firstAirline = '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef';
+    deployer.deploy(FlightSuretyData)
+    .then(() => {
+        return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
+                .then(() => {
+                    let config = {
+                        localhost: {
+                            url: 'http://localhost:8545',
+                            dataAddress: FlightSuretyData.address,
+                            appAddress: FlightSuretyApp.address
+                        }
+                    }
+                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                });
+    });
+}
